2d6aeb0f0f1675dea8461b51d9377137077d30d2
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2022-05-04 09:04:52 -0300

    aaa

---
+++ visitantes/src/pages/cadastroVisitantes.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { Component, React } from "react";
+import '../css/cadastroVisitantes.css';
+
+
+
+export default class cadastroVisitantes extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {};
+    }
+
+    render()
+    {
+        return(
+            <p>aaa</p>
+        )
+    }
+}
